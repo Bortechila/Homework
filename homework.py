d4417b899a3a71af511cf5b63903dc3e3abf20ef
Bortechila <111571243+Bortechila@users.noreply.github.com>
2022-08-20 16:03:19 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -6,16 +6,19 @@ from geometry_msgs.msg import Twist
 
 
 def pose_callback(msg: Pose):
-    cmd = Twist()
-    cmd.linear.x = 1.0
-    cmd.angular.z = 1.0
+
     message_1 = msg
     print(message_1.x, "\n", message_1.y, "\n\n")
-    pub.publish(cmd)
+    
 
 if __name__ == "__main__":
     rospy.init_node ("turtle_controller")
     pub = rospy.Publisher("/turtle1/cmd_vel", Twist, queue_size= 10)
     sub = rospy.Subscriber("/turtle1/pose", Pose, callback= pose_callback)
+    while not rospy.is_shutdown():
+        cmd = Twist()
+        cmd.linear.x = 1.0
+        cmd.angular.z = 5.0
+        pub.publish(cmd)
 
     rospy.spin()
