19965f451d7712799cbd50edd79327403fd6e967
Bortechila <111571243+Bortechila@users.noreply.github.com>
2022-08-30 21:18:52 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -6,24 +6,22 @@ import rospy
 
 
 vel = Twist()
-def cmd_vel_func():
-    vel.linear.x = 0.2 
-    vel.angular.z = 0.1
-    pub.publish(vel)
+vel.linear.x = 0.35
+vel.angular.z = 0
+    
 
 def vel_command_handler(req):
-    global vel
     vel.linear.x = req.x
     vel.angular.z = req.z
     return True
 
 
-rospy.Service("vel_commander", VelCommand, vel_command_handler)
+s = rospy.Service("vel_commander", VelCommand, vel_command_handler)
 
 if __name__ == "__main__":
     rospy.init_node("server", anonymous= True)
     r = rospy.Rate(1)
     pub = rospy.Publisher("/turtle1/cmd_vel", Twist, queue_size= 10)
     while not rospy.is_shutdown():
-        cmd_vel_func()
-    r.sleep()
+        pub.publish(vel)
+        r.sleep()
