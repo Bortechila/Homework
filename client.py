954277f6e4adbef2424f1cd89b9cba9e8f8f526e
Bortechila <111571243+Bortechila@users.noreply.github.com>
2022-08-30 21:18:35 +0300

    Add files via upload

--- client.py
+++ client.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python3
 
 import rospy
-import sys
 from first_pkg.srv import VelCommand
+
+
 
 rospy.init_node("client", anonymous= True)
 
-if __name__ == "__main__":
-    
-    x = float(sys.argv[1])
-    z = float(sys.argv[2])
 
+if __name__ == "__main__":
     rospy.wait_for_service("vel_commander")
     client = rospy.ServiceProxy("vel_commander", VelCommand)
-
-    client.call(x,z)
+    while 1 < 2:
+        x = 0
+        z = 0.393
+        client.call(x,z)
+        rospy.sleep(4)
+        x = 0.3
+        z = 0
+        client.call(x,z)
+        rospy.sleep(4)   
+    
+    rospy.spin()
+    
